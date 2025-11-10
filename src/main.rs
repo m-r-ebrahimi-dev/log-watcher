8dd14b02f67de688e2b1337e7f12734b49f36ccb
MmdRza.ebrahimi <moh.ebrahimi@bluteam.ir>
2025-11-10 16:52:50 +0330

    feat: Implement core file watching logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::path::Path;
+use notify::{Config, RecommendedWatcher, RecursiveMode, Watcher};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage: log-watcher <path_to_log_file>");
+        return Ok(());
+    }
+    let path_str = &args[1];
+    println!("---> Watching file: {}", path_str);
+
+    let (tx, rx) = std::sync::mpsc::channel();
+
+    let mut watcher = RecommendedWatcher::new(tx, Config::default())?;
+
+    watcher.watch(Path::new(path_str), RecursiveMode::NonRecursive)?;
+
+    println!("\n---> Waiting for file changes... (Press Ctrl+C to exit)");
+
+    for res in rx {
+        match res {
+            Ok(event) => {
+                if event.kind.is_modify() {
+                    println!("-> File modified! Next step: Read and parse.");
+                }
+            }
+            Err(e) => eprintln!("Watch error: {:?}", e),
+        }
+    }
+
+    Ok(())
 }
